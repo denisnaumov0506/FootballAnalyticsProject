e53767d7a48f7ba1e2f37dadcb851cc267967b00
uniWuerDenis <denis.naumov@stud-mail.uni-wuerzburg.de>
2022-08-30 19:42:25 +0200

    Added Fifaranking

--- FootballScraper2.R
+++ FootballScraper2.R
@@ -1,10 +1,5 @@
 # install necessary packages
 
-# THIS BIT IS NECESSARY WHEN FIRST INSTALLING RSTUDIO!!!
-# install.packages('installr')
-# library(installr)
-# install.Rtools()
-
 install.packages('jsonlite')
 install.packages('rvest')
 install.packages('tidyverse')
@@ -30,6 +25,19 @@ trimStringArray = function(array) {
   array
 }
 
+# ternaryOperatorFunction | Vectorized function
+
+ternary = function(expression_1, expression_2, condition) {
+  if(condition) {
+    expression_1
+  } else {
+    expression_2
+  }
+}
+
+# vectorized version
+
+ternary_v = Vectorize(ternary)
 
 scrapeColumnsWM = function(game_name) {
 
@@ -71,7 +79,7 @@ scrapeColumnsWM = function(game_name) {
       html_text() %>%
       trimStringArray() -> name
 
-    if (length(name) != 0 && grepl(tolower(game_name), tolower(name))) {
+    if (length(name) != 0 && tolower(game_name) == tolower(name)) {
       box = item
       break
     }
@@ -79,205 +87,194 @@ scrapeColumnsWM = function(game_name) {
 
   box
 
-  if(!is.null(box)) {
-      # Obtain the responsive_table
+  # Obtain the responsive_table
 
-      box %>%
-          html_elements('.responsive-table') -> responsive_table
+  box %>%
+    html_elements('.responsive-table') -> responsive_table
 
-      # print the responsive-table
+  # print the responsive-table
 
-      responsive_table
+  responsive_table
 
-      # scrape the url with the slugs and club ids of the countries
+  # scrape the url with the slugs and club ids of the countries
 
-      responsive_table %>%
-          html_elements('tr td:first-of-type a') %>%
-          html_attr('href') %>%
-          strsplit('/') -> raw_url_data
+  responsive_table %>%
+    html_elements('tr td:first-of-type a') %>%
+    html_attr('href') %>%
+    strsplit('/') -> raw_url_data
 
-      raw_url_data
+  raw_url_data
 
-      # obtain the slug of all countries
+  # obtain the slug of all countries
 
-      raw_url_data[1][[1]][2]
-      raw_url_data[1][[1]][length(raw_url_data[1][[1]])]
+  raw_url_data[1][[1]][2]
+  raw_url_data[1][[1]][length(raw_url_data[1][[1]])]
 
-      for (i in 1:length(raw_url_data)) {
-          slugs = c(slugs, raw_url_data[i][[1]][2])
-          club_ids = c(club_ids, raw_url_data[i][[1]][length(raw_url_data[1][[1]])])
-      }
-
-      # print slugs
-
-      slugs
-
-      # print club_ids
-
-      club_ids
-
-      # obtain the columnNames
-
-      responsive_table %>%
-          html_elements('th') %>%
-          html_text -> columnNames
-
-      # print columnNames
-
-      columnNames
-
-      # add slug and club_id to columnNames
-
-      columnNames = c(columnNames, c('slug', 'club_id'))
-
-      # print columnNames again
-
-      columnNames
-
-      # obtain length of a all true columns
-
-      responsive_table %>%
-          html_elements('tbody tr:nth-of-type(1) td') %>%
-          length() -> trueColumnsLength
-
-      # print the length of all true columns
-
-      trueColumnsLength
-
-      for (i in 1:trueColumnsLength) {
-          # create css selector string
-          # td:not([class*="no-border-rechts"]
-          string = paste0('tbody tr td:not([class*="no-border-rechts"]):nth-of-type(', i,')')
-
-          # print the css selector string
-
-          string
-
-          # obtain all row elements of specified column
-
-          responsive_table %>%
-              html_elements(string) -> html_node
-
-          # print all row elements of specified column
-
-          html_node
-
-          # only add the values if they are valid!!!
-
-          if(length(html_node) != 0) {
-              # obtain the children (if any) from the row elements
-              # of specified column
-
-              html_node %>%
-                  html_children() -> children
-
-              # print the children
-
-              children
-
-              # check the number of children contained within
-
-              num_of_children = length(children)
-
-              # print the number of children (if any)
-
-              num_of_children
-
-              # check if a child contains any subchildren
-
-              children %>%
-                  html_children() -> subchildren
-
-              # print subchildren
-
-              subchildren
-
-              # check the number of children contained within
-
-              num_of_subchildren = length(subchildren)
-
-              # print number of subchildren
-
-              num_of_subchildren
-
-              # dynammically apply a search for the values
-              # according to the number of children
-
-              if (num_of_subchildren > 0) {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_elements('a > span') %>%
-                      html_text() -> values
-              } else if (num_of_children > 0) {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_elements('a') %>%
-                      html_attr('title') -> values
-              } else {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_text -> values
-              }
-
-              # wrap the obtained values within a list
-
-              list(values)
-
-              # iterate over all children by
-
-              values = trimStringArray(values)
-
-              # Add this column to the columnValues list
-
-              columnValues = append(columnValues, list(values))
-          }
-          else {
-              print(paste0("Column ", i, " is not a valid value"))
-          }
-
-      }
-
-      # add club_id and slug to values
-
-      columnValues = append(columnValues, list(slugs))
-      columnValues = append(columnValues, list(club_ids))
-
-      # print columnValues
-
-      columnValues
-
-      # return list
-      list(columnNames, columnValues)
-  } else {
-      # return NULL
-      NULL
+  for (i in 1:length(raw_url_data)) {
+    slugs = c(slugs, raw_url_data[i][[1]][2])
+    club_ids = c(club_ids, raw_url_data[i][[1]][length(raw_url_data[1][[1]])])
   }
+
+  # print slugs
+
+  slugs
+
+  # print club_ids
+
+  club_ids
+
+  # obtain the columnNames
+
+  responsive_table %>%
+    html_elements('th') %>%
+    html_text -> columnNames
+
+  # print columnNames
+
+  columnNames
+
+  # add slug and club_id to columnNames
+
+  columnNames = c(columnNames, c('slug', 'club_id'))
+
+  # print columnNames again
+
+  columnNames
+
+  # obtain length of a all true columns
+
+  responsive_table %>%
+    html_elements('tbody tr:nth-of-type(1) td') %>%
+    length() -> trueColumnsLength
+
+  # print the length of all true columns
+
+  trueColumnsLength
+
+  for (i in 1:trueColumnsLength) {
+    # create css selector string
+    # td:not([class*="no-border-rechts"]
+    string = paste0('tbody tr td:not([class*="no-border-rechts"]):nth-of-type(', i,')')
+
+    # print the css selector string
+
+    string
+
+    # obtain all row elements of specified column
+
+    responsive_table %>%
+      html_elements(string) -> html_node
+
+    # print all row elements of specified column
+
+    html_node
+
+    # only add the values if they are valid!!!
+
+    if(length(html_node) != 0) {
+      # obtain the children (if any) from the row elements
+      # of specified column
+
+      html_node %>%
+        html_children() -> children
+
+      # print the children
+
+      children
+
+      # check the number of children contained within
+
+      num_of_children = length(children)
+
+      # print the number of children (if any)
+
+      num_of_children
+
+      # check if a child contains any subchildren
+
+      children %>%
+        html_children() -> subchildren
+
+      # print subchildren
+
+      subchildren
+
+      # check the number of children contained within
+
+      num_of_subchildren = length(subchildren)
+
+      # print number of subchildren
+
+      num_of_subchildren
+
+      # dynammically apply a search for the values
+      # according to the number of children
+
+      if (num_of_subchildren > 0) {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_elements('a > span') %>%
+          html_text() -> values
+      } else if (num_of_children > 0) {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_elements('a') %>%
+          html_attr('title') -> values
+      } else {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_text -> values
+      }
+
+      # wrap the obtained values within a list
+
+      list(values)
+
+      # iterate over all children by
+
+      values = trimStringArray(values)
+
+      # Add this column to the columnValues list
+
+      columnValues = append(columnValues, list(values))
+    }
+    else {
+      print(paste0("Column ", i, " is not a valid value"))
+    }
+
+  }
+
+  # add club_id and slug to values
+
+  columnValues = append(columnValues, list(slugs))
+  columnValues = append(columnValues, list(club_ids))
+
+  # print columnValues
+
+  columnValues
+
+  # return list
+  list(columnNames, columnValues)
 }
 
 create_dataframeWM = function(name) {
-  print(scrapeColumnsWM(name))
   df_wm = scrapeColumnsWM(name)
+  columns = df_wm[[2]]
+  columnNames = df_wm[[1]]
 
-  if(!(is.null(df_wm))) {
-      columns = df_wm[[2]]
-      columnNames = df_wm[[1]]
+  #print(columnNames)
 
-      #print(columnNames)
+  df_wm = data.frame(columns[[1]])
 
-      df_wm = data.frame(columns[[1]])
-
-      for (i in 2:length(columns)) {
-          df_wm[i] = columns[[i]]
-      }
-
-      names(df_wm) = columnNames[2:length(columnNames)]
-      df_wm
-  } else {
-      NULL
+  for (i in 2:length(columns)) {
+    df_wm[i] = columns[[i]]
   }
+
+  names(df_wm) = columnNames[2:length(columnNames)]
+  df_wm
 }
 
-df_wm = create_dataframeWM('teilnehmend teams an der wm')
+df_wm = create_dataframeWM('teilnehmende teams an der wm22')
 df_wm
 
 
@@ -333,7 +330,7 @@ scrapeColumns = function(year, country, club_id, game_name) {
         #print(name)
     }
 
-    if (length(name) != 0 && grepl(tolower(game_name), tolower(name))) {
+    if (length(name) != 0 && tolower(game_name) == tolower(name)) {
       box = item
       break
     }
@@ -341,164 +338,157 @@ scrapeColumns = function(year, country, club_id, game_name) {
 
   box
 
-  if(!is.null(box)) {
-      # Obtain the responsive_table
+  # Obtain the responsive_table
 
-      box %>%
-          html_elements('.responsive-table') -> responsive_table
+  box %>%
+    html_elements('.responsive-table') -> responsive_table
 
-      # print the responsive-table
+  # print the responsive-table
 
-      responsive_table
+  responsive_table
 
-      # obtain the columnNames
+  # obtain the columnNames
 
-      responsive_table %>%
-          html_elements('th') %>%
-          html_text -> columnNames
+  responsive_table %>%
+    html_elements('th') %>%
+    html_text -> columnNames
 
-      # print columnNames
+  # print columnNames
 
-      columnNames
+  columnNames
 
-      # obtain length of a all true columns
+  # obtain length of a all true columns
 
-      responsive_table %>%
-          html_elements('tbody tr:nth-of-type(1) td') %>%
-          length() -> trueColumnsLength
+  responsive_table %>%
+    html_elements('tbody tr:nth-of-type(1) td') %>%
+    length() -> trueColumnsLength
 
-      # print the length of all true columns
+  # print the length of all true columns
 
-      trueColumnsLength
+  trueColumnsLength
 
-      for (i in 1:trueColumnsLength) {
-          # create css selector string
-          # td:not([class*="no-border-rechts"]
-          string = paste0('tbody tr td:not([class*="no-border-rechts"]):nth-of-type(', i,')')
+  for (i in 1:trueColumnsLength) {
+    # create css selector string
+    # td:not([class*="no-border-rechts"]
+    string = paste0('tbody tr td:not([class*="no-border-rechts"]):nth-of-type(', i,')')
 
-          # print the css selector string
+    # print the css selector string
 
-          string
+    string
 
-          # obtain all row elements of specified column
+    # obtain all row elements of specified column
 
-          responsive_table %>%
-              html_elements(string) -> html_node
+    responsive_table %>%
+      html_elements(string) -> html_node
 
-          # print all row elements of specified column
+    # print all row elements of specified column
 
-          html_node
+    html_node
 
-          # only add the values if they are valid!!!
+    # only add the values if they are valid!!!
 
-          if(length(html_node) != 0) {
-              # obtain the children (if any) from the row elements
-              # of specified column
+    if(length(html_node) != 0) {
+      # obtain the children (if any) from the row elements
+      # of specified column
 
-              html_node %>%
-                  html_children() -> children
+      html_node %>%
+        html_children() -> children
 
-              # print the children
+      # print the children
 
-              children
+      children
 
-              # check the number of children contained within
+      # check the number of children contained within
 
-              num_of_children = length(children)
+      num_of_children = length(children)
 
-              # print the number of children (if any)
+      # print the number of children (if any)
 
-              num_of_children
+      num_of_children
 
-              # check if a child contains any subchildren
+      # check if a child contains any subchildren
 
-              children %>%
-                  html_children() -> subchildren
+      children %>%
+        html_children() -> subchildren
 
-              # print subchildren
+      # print subchildren
 
-              subchildren
+      subchildren
 
-              # check the number of children contained within
+      # check the number of children contained within
 
-              num_of_subchildren = length(subchildren)
+      num_of_subchildren = length(subchildren)
 
-              # print number of subchildren
+      # print number of subchildren
 
-              num_of_subchildren
+      num_of_subchildren
 
-              # dynammically apply a search for the values
-              # according to the number of children
+      # dynammically apply a search for the values
+      # according to the number of children
 
-              if (num_of_subchildren > 0) {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_elements('a > span') %>%
-                      html_text() -> values
-              } else if (num_of_children > 0) {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_elements('a') %>%
-                      html_attr('title') -> values
-              } else {
-                  responsive_table %>%
-                      html_elements(string) %>%
-                      html_text -> values
-              }
-
-              # wrap the obtained values within a list
-
-              list(values)
-
-              # iterate over all children by
-
-              values = trimStringArray(values)
-
-              # Add this column to the columnValues list
-
-              columnValues = append(columnValues, list(values))
-          }
-          else {
-              print(paste0("Column ", i, " is not a valid value"))
-          }
-
+      if (num_of_subchildren > 0) {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_elements('a > span') %>%
+          html_text() -> values
+      } else if (num_of_children > 0) {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_elements('a') %>%
+          html_attr('title') -> values
+      } else {
+        responsive_table %>%
+          html_elements(string) %>%
+          html_text -> values
       }
 
-      # return list
-      list(columnNames, columnValues)
-  } else {
-      NULL
+      # wrap the obtained values within a list
+
+      list(values)
+
+      # iterate over all children by
+
+      values = trimStringArray(values)
+
+      # Add this column to the columnValues list
+
+      columnValues = append(columnValues, list(values))
+    }
+    else {
+      print(paste0("Column ", i, " is not a valid value"))
+    }
+
   }
+
+  # return list
+  list(columnNames, columnValues)
+
+
 }
 
 createDataframe = function(year, country, club_id, game_name) {
 
   list = scrapeColumns(year, country, club_id, game_name)
+  print("test")
+  print(list)
+  columns = list[[2]]
+  columnNames = list[[1]]
 
-  if(!is.null(list)) {
-      print("test")
-      print(list)
-      columns = list[[2]]
-      columnNames = list[[1]]
+  #print(columns)
+  #print(columnNames)
 
-      #print(columns)
-      #print(columnNames)
+  # create a dataframe with first colum and append the rest (workaround)
+  df_test = data.frame(columns[[1]])
 
-      # create a dataframe with first colum and append the rest (workaround)
-      df_test = data.frame(columns[[1]])
-
-      for (i in 2:length(columns)) {
-          df_test[i] = columns[[i]]
-      }
-
-      names(df_test) = columnNames
-      df_test
-  } else {
-      NULL
+  for (i in 2:length(columns)) {
+    df_test[i] = columns[[i]]
   }
+
+  names(df_test) = columnNames
+  df_test
 }
 
 # Testing Section
 
-df_new_test = createDataframe(2013, 'spanien', 3375, 'world cup')
+df_new_test = createDataframe(2013, 'spanien', 3375, 'world cup 2010')
 df_new_test
